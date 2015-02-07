eb07ebfc1e1b065e94cbccc2a594c78ba918f40c
David Furstein <david.furstein@gmail.com>
2015-02-07 13:38:19 -0500

    small adjustments

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 # Controller for the main index page
 class MainController < ApplicationController
   def index
-    @date = Date.today.to_formatted_s(:rfc822)
+    @date = Date.today.to_formatted_s(:rfc822).upcase
 
     client = Weatherman::Client.new unit: 'F'
     response = client.lookup_by_woeid 2459115
