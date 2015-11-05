52c8c159e1318a4e08f2b02e0fa62c4b8079309a
David Furstein <david.furstein@gmail.com>
2015-11-04 23:41:44 -0500

    fixed bug in location hours code

--- app/helpers/location_hour_helper.rb
+++ app/helpers/location_hour_helper.rb
@@ -11,7 +11,7 @@ module LocationHourHelper
       "#{Date::DAYNAMES[hours.keys[0].sort.first]} – #{Date::DAYNAMES[hours.keys[0].sort.last]}, #{format_time(hours.values[0][0])} – #{format_time(hours.values[0][1])}"
     elsif hours.keys.count == 2 && ((consecutive_days?(hours.keys[0].sort) && hours.keys[1] == [0, 6]) ||
       (consecutive_days?(hours.keys[1].sort) && hours.keys[0] == [0, 6]))
-      "#{Date::DAYNAMES[hours.keys[0].sort.first]} – #{Date::DAYNAMES[hours.keys[0].sort.last]}, #{format_time(hours.values[0][0])} – #{format_time(hours.values[0][1])}<br />Weekends, #{format_time(hours.values[0][0])} – #{format_time(hours.values[0][1])}".html_safe
+      "#{Date::DAYNAMES[hours.keys[0].sort.first]} – #{Date::DAYNAMES[hours.keys[0].sort.last]}, #{format_time(hours.values[0][0])} – #{format_time(hours.values[0][1])}<br />Weekends, #{format_time(hours.values[1][0])} – #{format_time(hours.values[1][1])}".html_safe
     end
   end
 
